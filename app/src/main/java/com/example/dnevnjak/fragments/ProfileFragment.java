93a57c86d8fbb3da72fcf2979ea034b04f16fa1d
Aleksa Buncic <alextromb1@gmail.com>
2023-04-20 10:31:17 +0200

    Profile tab

--- app/src/main/java/com/example/dnevnjak/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/dnevnjak/fragments/ProfileFragment.java
@@ -11,11 +11,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.dnevnjak.R;
 
 public class ProfileFragment extends Fragment {
-
+    private TextView usernameText;
+    private Button changePasswordButton;
+    private Button logoutButton;
     private ProfileViewModel mViewModel;
 
     public static ProfileFragment newInstance() {
